c5ab7d222f6ee6a978de56217023a7bc7e285fad
victorfernandesraton <vfbraton@gmail.com>
2020-09-13 03:01:42 -0300

    implementing route

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 var router = require('express').Router();
 
 const User = require('./user/');
+const Publication = require('./publication/');
 
 router.use('/user',User.Router);
+router.use('/pub',Publication.Router);
 
 module.exports = router;

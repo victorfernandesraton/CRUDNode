9bbb374cdda0feca42d343d6f270993cc1d0a277
borgeswvictor <borgesvictormachado@gmail.com>
2020-09-12 21:59:55 -0300

    codigo editora

---
+++ editora/editoraRoute.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+const EditoraService = require("./editoraService");
+
+router.get("/", (req, res, send) => {
+    try {
+        const data = await EditoraService.index({});
+        res.render("show/editora", { data });
+    }catch (error) {
+        send(error);
+    }
+});
+
+router.get("/create", (req, res, send) => {
+    res.render("create/editora");
+});
+
+router.get("/delete/:id", async (req, res, send) => {
+    const { id } = req.params;
+    if (!id) {
+        send(404, {
+            error: "id is required",
+        });
+    }
+    try {
+        const data = await EditoraService.deleteOne({ id });
+        res.redirect("/user");
+    } catch (error) {
+        if (error.message.includes("user not found")) {
+            res.status(404).json({
+                data: "not found",
+            });
+        } else {
+            send(error);
+        }
+    }
+});
+
+router.get("/edit/:id", async (req, res, send) => {
+    const { id } = req.params;
+    if(!id) {
+        send(404, {
+            error: "id is required",
+        });
+    }
+    try {
+        const data = await EditoraService.findOne({ id });
+        res.render("edit/editora", { data });
+    }catch (error) {
+        if (error.message.includes("editora not found")) {
+            res.status(404).json({
+                data: "not found",
+            });
+        } else {
+            send(error);
+        }
+    }
+});
+
+router.post("/:id", async (req, res, send) => {
+    const { id } = req.params;
+    if (!id) {
+        send(404, {
+            error: "id is required",
+        });
+    }
+    try {
+        const record = await EditoraService.updateOne({
+            id,
+            user: { ...req.body },
+        });
+        res.redirect(`/user?u=${id}`);
+    } catch (error) {
+        send(error);
+    }
+});
+
+router.post("/", async (req,res, send) => {
+    try {
+        const record = await EditoraService.store({ editora: { ...req.body } });
+        const data = await EditoraService.index({});
+        res.render("show/editora", { data });
+    } catch (error) {
+        send(error);
+    }
+});
+
+module.exports = router;
---
+++ editora/editoraService.js
@@ -0,0 +1,177 @@
+const conection = require("..database/mongodb.config");
+const mongodb = require("mongodb");
+
+const validator = require("validator");
+const { isDate, isAfter } = require("date-fns");
+
+const { Cep, Cnpj, Razao, Phone, Cpf } = require("../validation/");
+
+const validate = ({
+    mail,
+    razao,
+    cnpj,
+    phone,
+}) => {
+    if (!cpnj) {
+        return new Error(`cnpj is required`);
+    }
+
+    if (!Cnpj(cnpj)) {
+        return new Error(`cnpj not valid ${cnpj}`);
+    } 
+
+    if (!razao) {
+        return new Error(`razao is required ${razao}`);
+    }
+
+    if (!razao) {
+        return new Error(`razao not valid ${razao}`);
+    }
+}
+
+const index = async ({ limit = 10, offset = 0, filter = {} }) => {
+    const client = await conection();
+    if (filter._id) {
+        filter._id = mongodb.ObjectID(filter._id);
+    }
+    try {
+        const collection = client.db("api").collection("editora");
+        const data = await collection
+            .find({ ...filter })
+            .skip(offset)
+            .limit(limit)
+            .toArray();
+        return data;
+    } catch (error) {
+        throw new Error(error);
+    }finally {
+        await client.close();
+    }
+};
+
+const store =  async ({ editora = null}) => {
+    if (!editora) {
+        throw new Error("editora is required");
+    }
+    const isValud = validate({ ...editora});
+
+    if (isValud) {
+        throw new Error(isValud)
+    }
+
+    try {
+        const client = await conection();
+        try {
+            const cllection = client.db("api").collection("editora");
+            const record = await collection.insertOne({ ...editora });
+            const data = await index({});
+            return data;
+        } catch (error) {
+            throw new Error(error);
+        } finally {
+            client.close();
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const findOne = async ({ id }) => {
+    if (!id) {
+        throw new Error("id is required");
+    }
+
+    try {
+        const findRecord = await index({
+            filter: { _id: id },
+        });
+        if (!findRecord.length) {
+            throw new Error("user not found");
+        }
+        return findRecord;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const updateOne = async ({ id, editora }) => {
+    if (!id) {
+        throw new Error("id is required");
+    }
+    if (!editora) {
+        throw new Error("editora is required");
+    }
+    let findRecord;
+
+    try {
+        findRecord = await findOne({ id });
+    } catch (error) {
+        throw new Error(error);
+    }
+
+    if (!findRecord.length) {
+        throw new Error("editora not found");
+    }
+
+    try {
+        const client = await conection();
+        try {
+            const updateRecord = await client
+                .db("api")
+                .collection("editora")
+                .updateOne(
+                    { _id: mongodb.ObjectID(id) },
+                    { $set: editora },
+                    { upsert: true }
+                );
+            return updateRecord; 
+        } catch (error) {
+            throw new Error(error);
+        } finally {
+            client.close();
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const deleteOne = async ({ id }) => {
+    if (!id) {
+        throw new Error("id is required");
+    }
+    try {
+        findRecord = await findOne({ id });
+    } catch (error) {
+        throw new Error(error);
+    }
+
+    if (!findRecord.length) {
+        throw new Error("editora not found");
+    }
+
+    try {
+        const client = await conection();
+        try {
+            const deleteRecord = await client
+                .db("api")
+                .collection("editora")
+                .deleteOne({ _id: mongodb.ObjectID(id) });
+            return deleteRecord;
+        } catch (error) {
+            throw new Error(error);
+        } finally {
+            client.close();
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+module.exports = {
+    index,
+    store,
+    findOne,
+    updateOne,
+    deleteOne,
+    validate,
+};

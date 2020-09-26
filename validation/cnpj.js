966c2c9e41f448ed87b3407a6ee22b7c3a585e9b
victorfernandesraton <vfbraton@gmail.com>
2020-09-26 16:00:08 -0300

    fix cnpj error

--- validation/cnpj.js
+++ validation/cnpj.js
@@ -1,3 +1,3 @@
-const cnpj = /^\d{2}\d{3}\d{3}\d{3}\d{2}$/;
+const cnpj = /^\d{2}\d{3}\d{3}\d{4}\d{2}$/;
 
-module.exports = (v) => cnpj.test(v.replace('[^0-9\s.]+|\.(?!\D)', ''))
+module.exports = (v) => cnpj.test(v.replace(/\D+/g, ''))
--- validation/index.js
+++ validation/index.js
@@ -3,11 +3,13 @@ const CepExtractor = require('./cepExtractor');
 const Cpf = require('./cpf');
 const Name = require('./name');
 const Phone = require('./phone');
+const Cnpj = require('./cnpj');
 
 module.exports ={
     Cep,
     CepExtractor,
     Cpf,
     Name,
-    Phone
+    Phone,
+    Cnpj
 }
